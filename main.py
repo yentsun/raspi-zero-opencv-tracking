434535e752720dddefb9271ac26626a73a6080db
Max Korinets <mkorinets@gmail.com>
2018-03-08 15:20:46 +0300

    Add comments

--- main.py
+++ main.py
@@ -12,58 +12,53 @@ THRESHOLD = int(os.getenv('THRESHOLD', 75))
 ACTION_URL = os.getenv('ACTION_URL', 'http://localhost/action')
 STILL_URL = os.getenv('STILL_URL', 'http://localhost/still')
 
-camera = cv2.VideoCapture(DEVICE)
+camera = cv2.VideoCapture(DEVICE)  # open video capture stream from the camera
 time.sleep(0.25)
 firstFrame = None
 action_saved = False
 action_count = 1
 
-while True:
-    # grab the current frame
-    (grabbed, frame) = camera.read()
+while True:  # start endless loop
+
+    (grabbed, frame) = camera.read()  # grab a frame from the stream
     text = "still"
 
     if not grabbed:
         break
 
-    # resize the frame, convert it to grayscale, and blur it
     frame = imutils.resize(frame, width=500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # create a grayscale frame
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-    # if the first frame is None, initialize it
-    if firstFrame is None:
+    if firstFrame is None:  # initialize the first frame
         firstFrame = gray
         continue
 
-    # compute the absolute difference between the current frame and
-    # first frame
-    frameDelta = cv2.absdiff(firstFrame, gray)
-    thresh = cv2.threshold(frameDelta, THRESHOLD, 255, cv2.THRESH_BINARY)[1]
-
-    # dilate the thresholded image to fill in holes, then find contours
-    # on thresholded image
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+    frameDelta = cv2.absdiff(firstFrame, gray)  # compute difference between the first frame and current one
+    thresh = cv2.threshold(frameDelta, THRESHOLD, 255, cv2.THRESH_BINARY)[1]  # create thresholded image
+    thresh = cv2.dilate(thresh, None, iterations=2)  # dilate thresholded image to fill in the gaps
+    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,  # find contours
                                  cv2.CHAIN_APPROX_SIMPLE)
 
-    # loop over the contours
     for c in cnts:
         # if the contour is too small, ignore it
         if cv2.contourArea(c) < MIN_AREA:
             continue
 
-        # compute the bounding box for the contour, draw it on the frame,
-        # and update the text
+        # draw bounding box over the contours on the original frame
         (x, y, w, h) = cv2.boundingRect(c)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         text = "car #%d" % action_count
 
-    # draw the text and timestamp on the frame
+    # stamp datetime and other info onto the original frame
     cv2.putText(frame, "Scene Status: {}".format(text), (10, 20),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p ") + "T: %d" % THRESHOLD,
                 (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+    # if current frame contains boxed out contours (i.e.there is some
+    # action), print the number, POST it to an 'action' HTTP endpoint and save
+    # the image to filesystem
     if text != 'still' and not action_saved:
         print('action frame', action_count)
         cv2.imwrite("action%d.png" % action_count, frame)
@@ -74,6 +69,8 @@ while True:
         except requests.exceptions.ConnectionError as error:
             print(error)
         action_saved = True
+
+    # otherwise POST it to a 'still' (no action) HTTP endpoint, for the live feed
     if text == 'still':
         try:
             headers = {'content-type': 'image/png'}
@@ -81,7 +78,7 @@ while True:
             requests.post(STILL_URL, data=img_encoded.tostring(), headers=headers)
         except requests.exceptions.ConnectionError as error:
             print(error)
-        if action_saved:
+        if action_saved:  # if a still frame follows an action frame - increment the counter
             print('object %d tracked' % action_count)
             action_count += 1
             action_saved = False
